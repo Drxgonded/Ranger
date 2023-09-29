bccacf97a652f9d27c513a3bb5f91981d1e59a23
Matthew Schaefer <drxgonded@outlook.com>
2023-09-29 17:13:29 -0400

    Add more code to the vending machine

--- kfc.cpp
+++ kfc.cpp
@@ -1,8 +1,59 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <string>
 
-int main()
-{
-    cout << "HELLO";
-    return 0;
+// Define a structure to represent a fruit
+struct Fruit {
+    std::string name;
+    double weight;  // in grams
+    double price;   // per kilogram
+};
+
+// Define a class to manage fruit-related functions
+class FruitFunctions {
+public:
+    // Constructor
+    FruitFunctions() {
+        // Initialize the array of function pointers with fruit-related functions
+        functions[0] = &FruitFunctions::calculateTotalCost;
+        functions[1] = &FruitFunctions::calculateCalories;
+        functions[2] = &FruitFunctions::calculateVitaminC;
+    }
+
+    // Function to calculate the total cost of buying a given quantity of fruit
+    static double calculateTotalCost(const Fruit& fruit, int quantity) {
+        return (fruit.price / 1000) * fruit.weight * quantity;
+    }
+
+    // Function to calculate the calories in a given quantity of fruit
+    static double calculateCalories(const Fruit& fruit, int quantity) {
+        return (fruit.weight / 100) * 52 * quantity; // Assuming 52 calories per 100 grams
+    }
+
+    // Function to calculate the vitamin C content in a given quantity of fruit
+    static double calculateVitaminC(const Fruit& fruit, int quantity) {
+        return (fruit.weight / 100) * 50 * quantity; // Assuming 50 mg of vitamin C per 100 grams
+    }
+
+    // Function to execute a selected fruit-related function
+    double executeFunction(int choice, const Fruit& fruit, int quantity) {
+        if (choice >= 1 && choice <= 3) {
+            // Call the selected function and return the result
+            return (this->functions[choice - 1])(fruit, quantity);
+        } else {
+            // Invalid choice
+            return -1.0;
+        }
+    }
+    
+private:
+    // Array of function pointers
+    double (FruitFunctions::functions[3])(const Fruit&, int);
+};
+
+int main() {
+    // Define a fruit
+    Fruit apple = {"Apple", 150, 2.0}; // 150g apple for $2.00 per kilogram
+
+    // Create an instance of the FruitFunctions class
+    FruitFunctions fruitFunc;
 }
--- popeyes.cpp
+++ popeyes.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
 
-int main()
-{
-  int choice;
+
+//Function that makes the user select options 1-4 and asks them again if they dont
+int askUserForNumber(int choice) {
+int userChoice;
+string option = "Choose an option (1-4): ";
+do { cout << option << endl;
+    cin >> userChoice; cout << "Invalid choice. Please select another";
+} while (userChoice != choice);
+
+
+}
+
+
+int main() {
+
+int choice = (1 <= 4);
+int userChoice = askUserForNumber(choice);
 
   cout << "*Select The Item You Want*" << endl; 
   cout << "1. Fried Chicken" << endl; 
@@ -11,204 +25,30 @@ int main()
   cout << "3. Cotton" << endl;
   cout << "4. Mac n Cheese" << endl;
 
-    return 0;
-}
 
+  cout << "Choose an option (1-4): " << endl;
+  cin >> userChoice;
+askUserForNumber(choice);
 
+    switch (userChoice) {
 
+     case 1:
+         cout << "Do you want to buy Fried Chicken? " << endl;
+         break;
+     case 2:
+         cout << "Do you want to buy Kool-Aid? " << endl;
+         break;
+     case 3:
+         cout << "Do you want to buy Cotton? " << endl;
+         break;
+     case 4: 
+         cout << "Do you want to buy Mac n Cheese? " << endl;
+         break;
+     default: 
+         cout << "Please choose a number from 1 to 4." << endl;
+         break;
+     }
 
-
-
-
-#include <iostream>
-
-int main() {
-    int choice;
-
-    std::cout << "Choose an option (1-3): ";
-    std::cin >> choice;
-
-    switch (choice) {
-        case 1:
-            std::cout << "You chose option 1." << std::endl;
-            break;
-        case 2:
-            std::cout << "You chose option 2." << std::endl;
-            break;
-        case 3:
-            std::cout << "You chose option 3." << std::endl;
-            break;
-        default:
-            std::cout << "Invalid choice. Please choose between 1 and 3." << std::endl;
-            break;
-    }
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-#include <iostream>
-
-int main() {
-    int choice;
-
-    std::cout << "Choose an option (1-3): ";
-    std::cin >> choice;
-
-    if (choice == 1) {
-        std::cout << "You chose option 1." << std::endl;
-    } else if (choice == 2) {
-        std::cout << "You chose option 2." << std::endl;
-    } else if (choice == 3) {
-        std::cout << "You chose option 3." << std::endl;
-    } else {
-        std::cout << "Invalid choice. Please choose between 1 and 3." << std::endl;
-    }
-
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#include <iostream>
-
-// Function declaration
-int add(int a, int b) {
-    return a + b;
-}
-
-int main() {
-    int num1 = 5, num2 = 3;
-
-    // Function call
-    int sum = add(num1, num2);
-
-    std::cout << "Sum: " << sum << std::endl;
-
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-#include <iostream>
-
-// Function declaration with default parameter
-int multiply(int a, int b = 2) {
-    return a * b;
-}
-
-int main() {
-    int num1 = 5;
-
-    // Function call with one argument
-    int result1 = multiply(num1);
-
-    // Function call with two arguments
-    int result2 = multiply(num1, 3);
-
-    std::cout << "Result 1: " << result1 << std::endl;
-    std::cout << "Result 2: " << result2 << std::endl;
-
-    return 0;
-}
-
-
-
-
-
-
-
-
-#include <iostream>
-
-// Recursive function to calculate factorial
-unsigned long long factorial(int n) {
-    if (n == 0 || n == 1) {
-        return 1;
-    }
-    return n * factorial(n - 1);
-}
-
-int main() {
-    int num = 5;
-    unsigned long long fact = factorial(num);
-
-    std::cout << "Factorial of " << num << " is " << fact << std::endl;
-
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#include <iostream>
-
-// Function overloading
-int add(int a, int b) {
-    return a + b;
-}
-
-double add(double a, double b) {
-    return a + b;
-}
-
-int main() {
-    int num1 = 5, num2 = 3;
-    double num3 = 2.5, num4 = 4.2;
-
-    int sum_int = add(num1, num2);
-    double sum_double = add(num3, num4);
-
-    std::cout << "Sum (int): " << sum_int << std::endl;
-    std::cout << "Sum (double): " << sum_double << std::endl;
-
-    return 0;
-}
-
-
-
